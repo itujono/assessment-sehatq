92479aaa87d4437cffe3046ccc8cb8abb4a76ea4
riva <rivayudha@gmail.com>
2020-03-05 01:58:10 +0700

    Bottombar @ Home.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -34,6 +34,16 @@ const ListItem = styled(List.Item)`
     }
 `
 
+const BottomBar = styled(Row)`
+    text-align: center;
+    position: sticky;
+    bottom: 0;
+    background-color: #f3f3f3;
+    .ant-col {
+        padding: 1.5em 1em;
+    }
+`
+
 export default function Home() {
     const dispatch = useDispatch()
     const categories = useSelector(({ product }) => product.categories)
@@ -45,34 +55,42 @@ export default function Home() {
     }, [])
 
     return (
-        <Section>
-            <Row gutter={16} className="mb2em">
-                <Col xs={4}>{/* <StarOutline /> */}</Col>
-                <Col xs={20}>
-                    <Input.Search placeholder="Search everything..." onSearch={value => console.log({ value })} />
-                </Col>
-            </Row>
-
-            <ScrollerRow type="flex" gutter={16}>
-                {categories.map(item => (
-                    <Col xs={6} className="ta-center">
-                        <div>
-                            <Avatar src={item.imageUrl} />
-                        </div>
-                        <Typography.Text>{item.name}</Typography.Text>
+        <>
+            <Section>
+                <Row gutter={16} className="mb2em">
+                    <Col xs={4}>{/* <StarOutline /> */}</Col>
+                    <Col xs={20}>
+                        <Input.Search placeholder="Search everything..." onSearch={value => console.log({ value })} />
                     </Col>
-                ))}
-            </ScrollerRow>
+                </Row>
 
-            <List
-                itemLayout="vertical"
-                dataSource={promoProducts}
-                renderItem={item => (
-                    <ListItem key={item.id} extra={<img src={item.imageUrl} width="100%" alt={item.title} />}>
-                        {item.title}
-                    </ListItem>
-                )}
-            />
-        </Section>
+                <ScrollerRow type="flex" gutter={16}>
+                    {categories.map(item => (
+                        <Col xs={6} className="ta-center">
+                            <div>
+                                <Avatar src={item.imageUrl} />
+                            </div>
+                            <Typography.Text>{item.name}</Typography.Text>
+                        </Col>
+                    ))}
+                </ScrollerRow>
+
+                <List
+                    itemLayout="vertical"
+                    dataSource={promoProducts}
+                    renderItem={item => (
+                        <ListItem key={item.id} extra={<img src={item.imageUrl} width="100%" alt={item.title} />}>
+                            {item.title}
+                        </ListItem>
+                    )}
+                />
+            </Section>
+            <BottomBar type="flex" justify="center">
+                <Col xs={6}>Home</Col>
+                <Col xs={6}>Feed</Col>
+                <Col xs={6}>Cart</Col>
+                <Col xs={6}>Profile</Col>
+            </BottomBar>
+        </>
     )
 }
